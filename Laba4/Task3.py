51d518fc0d9468c5cb79e72d05d999834a267f7d
shvertick-spbstu <115480529+shvertick-spbstu@users.noreply.github.com>
2022-10-30 15:16:17 +0300

    Лабораторная работа 4

---
+++ Laba4/Task3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+    if index is None:
+        list_ = list_[:len(list_)-1]
+        return list_
+    list1 = list_[:index]
+    list2 = list_[index+1:]
+    list_ = list1 + list2
+    return list_
+
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
