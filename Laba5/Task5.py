8c1fdbe0bbd6929e55374eaf673041569822df49
shvertick-spbstu <115480529+shvertick-spbstu@users.noreply.github.com>
2022-11-13 15:59:29 +0300

    Update Task5.py

--- Laba5/Task5.py
+++ Laba5/Task5.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def get_random_password(n=8, uniq=2) -> str: #uniqe - максимальное количство дублей символа в пароле
+def get_random_password(n=8, uniq=2) -> str: #uniq - максимальное количство дублей символа в пароле
     symbols = string.ascii_letters + string.digits
     password = random.sample(symbols, n, counts=[uniq for _ in symbols])
     return "".join(password)
