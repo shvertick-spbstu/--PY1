28bd0061bbdf4bba051c3997f76a79bf881d1f85
shvertick-spbstu <115480529+shvertick-spbstu@users.noreply.github.com>
2022-11-27 20:00:52 +0300

    Лабораторная работа 6

---
+++ Laba6/Task4/Task4.py
@@ -0,0 +1,26 @@
+import json
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(file_name) -> list[dict]:
+    with open(file_name, encoding="utf-8") as f:
+        data = f.readlines()
+        counter = -1
+        print(data)
+        for row in data:
+            row = row[:-1] #удаляю символ \n
+            row = row.split(",") #преобразовываю строку в список строк
+            counter += 1
+            data[counter] = row #перезапись
+    head = data[0]
+    data = data[1:]
+    result = []
+    for item in data:
+        dict_ = {k: v for k, v in zip(head, item)}
+        result.append(dict_)
+    return result
+
+
+csv_to_list_dict(INPUT_FILE)
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
